3fbaa5a343e164503d8d9b118437fb15f8097052
Caleb Ginn <caleb.ginn@gmail.com>
2023-08-03 16:16:44 -0500

    move providers and variables to own file, implement azure key vault and use key to create a user

--- main.tf
+++ main.tf
@@ -1,9 +1,66 @@
-terraform {
-  required_providers {
-    snowflake = {
-      source  = "Snowflake-Labs/snowflake"
-      version = "~> 0.68"
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = random_pet.rg_name.id
+  location = var.resource_group_location
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "random_string" "azurerm_key_vault_name" {
+  length  = 13
+  lower   = true
+  numeric = false
+  special = false
+  upper   = false
+}
+
+locals {
+  current_user_id = coalesce(var.msi_id, data.azurerm_client_config.current.object_id)
+}
+
+resource "azurerm_key_vault" "vault" {
+  name                       = coalesce(var.vault_name, "vault-${random_string.azurerm_key_vault_name.result}")
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = var.sku_name
+  soft_delete_retention_days = 7
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = local.current_user_id
+
+    key_permissions    = var.key_permissions
+    secret_permissions = var.secret_permissions
+  }
+}
+
+resource "random_string" "azurerm_key_vault_key_name" {
+  length  = 13
+  lower   = true
+  numeric = false
+  special = false
+  upper   = false
+}
+
+resource "azurerm_key_vault_key" "key" {
+  name = coalesce(var.key_name, "key-${random_string.azurerm_key_vault_key_name.result}")
+
+  key_vault_id = azurerm_key_vault.vault.id
+  key_type     = var.key_type
+  key_size     = var.key_size
+  key_opts     = var.key_ops
+
+  rotation_policy {
+    automatic {
+      time_before_expiry = "P30D"
     }
+
+    expire_after         = "P90D"
+    notify_before_expiry = "P29D"
   }
 }
 
@@ -66,18 +123,13 @@ resource "snowflake_grant_privileges_to_role" "warehouse_grant" {
   }
 }
 
-resource "tls_private_key" "svc_key" {
-  algorithm = "RSA"
-  rsa_bits  = 2048
-}
-
 resource "snowflake_user" "user" {
     provider          = snowflake.security_admin
     name              = "tf_demo_user"
     default_warehouse = snowflake_warehouse.warehouse.name
     default_role      = snowflake_role.role.name
     default_namespace = "${snowflake_database.db.name}.${snowflake_schema.schema.name}"
-    rsa_public_key    = substr(tls_private_key.svc_key.public_key_pem, 27, 398)
+    rsa_public_key    = substr(azurerm_key_vault_key.key.public_key_pem, 27, 398)
 }
 
 resource "snowflake_grant_privileges_to_role" "user_grant" {
--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,20 @@
 output "snowflake_svc_public_key" {
-    value = tls_private_key.svc_key.public_key_pem
+  value = tls_private_key.svc_key.public_key_pem
 }
 
 output "snowflake_svc_private_key" {
-    value     = tls_private_key.svc_key.private_key_pem
-    sensitive = true
+  value     = tls_private_key.svc_key.private_key_pem
+  sensitive = true
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "azurerm_key_vault_name" {
+  value = azurerm_key_vault.vault.name
+}
+
+output "azurerm_key_vault_id" {
+  value = azurerm_key_vault.vault.id
 }
